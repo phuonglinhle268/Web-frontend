fa0d5488c8d03ce0d29e232294d6ae7a2dd91e18
phuonglinhle268 <plinhle26@gmail.com>
2025-02-26 11:43:52 +0700

    ss16

--- session 16/ss16-10.js
+++ session 16/ss16-10.js
@@ -2,5 +2,5 @@ var num1 = parseInt(prompt("Nhập số thứ nhất"));
 var num2 = parseInt(prompt("Nhập số thứ hai"));
 var min = Math.min(num1, num2);
 var max = Math.max(num1, num2);
-var randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+var randomNum = Math.floor(Math.random() * (max - min - 2 + 1)) + min + 1;
 alert("Số ngẫu nhiên trong khoảng " + min + " đến " + max + " là: " + randomNum);
